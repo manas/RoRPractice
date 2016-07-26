4189e67bd91e55045f52c9af0ff1496551e2343d
Manas Agrawal <manas.agrawal@hotmail.com>
2016-07-25 21:28:58 -0400

    database setup changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723215051) do
+ActiveRecord::Schema.define(version: 20160722042038) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "words", force: :cascade do |t|
     t.string   "word"
